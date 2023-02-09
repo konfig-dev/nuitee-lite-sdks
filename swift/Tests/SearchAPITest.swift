30065a71273a77efadce1f80b9d1b2ac6cb426fa
dphuang2 <dylan.p.huang@gmail.com>
2023-02-09 15:00:07 -0800

    all operations tested

--- swift/Tests/SearchAPITest.swift
+++ swift/Tests/SearchAPITest.swift
@@ -9,11 +9,13 @@ import Foundation
 import XCTest
 
 class SearchAPITest: XCTestCase {
+    let dateFormatter = DateFormatter()
     override func setUpWithError() throws {
         let apiKey = ProcessInfo.processInfo.environment["NUITEE_API_KEY"]!
         NuiteeAPI.customHeaders = [
             "X-API-Key": apiKey,
         ]
+        dateFormatter.dateFormat = "yyyy-MM-dd"
     }
 
     override func tearDownWithError() throws {
@@ -22,13 +24,10 @@ class SearchAPITest: XCTestCase {
 
     func testGetHotels() throws {
         let expectation = XCTestExpectation(description: "Response is not null")
-        let now = Date()
-        var dateComponent = DateComponents()
-        dateComponent.month = 2
-        let checkin = Calendar.current.date(byAdding: dateComponent, to: now)!
-        dateComponent.month = 4
-        let checkout = Calendar.current.date(byAdding: dateComponent, to: now)!
-        SearchAPI.getHotels(checkin: checkin, checkout: checkout, country: "US", adults: 1, currency: "USD", guestNationality: "US") { response, error in
+        let checkin = dateFormatter.date(from: "2023-02-15")
+        let checkout = dateFormatter.date(from: "2023-02-16")
+        let hotelIds = "1000018,26191,248093,57871,268206,28906,497829,436827,1000091,1000876,1001301,1001325,1001464,99249,99122,99121,99119"
+        SearchAPI.getHotels(hotelIds: hotelIds, checkin: checkin!, checkout: checkout!, country: "US", adults: 1, currency: "USD", guestNationality: "US") { response, error in
             guard error == nil else {
                 print(error!)
                 return
@@ -38,6 +37,24 @@ class SearchAPITest: XCTestCase {
                 expectation.fulfill()
             }
         }
-        wait(for: [expectation], timeout: 10.0)
+        wait(for: [expectation], timeout: 5.0)
+    }
+
+    func testGetHotelRates() throws {
+        let expectation = XCTestExpectation(description: "Response is not null")
+        let checkin = dateFormatter.date(from: "2023-03-15")
+        let checkout = dateFormatter.date(from: "2023-03-16")
+        let hotelIds = "57871,26191,248093,268206,28906,497829,436827"
+        SearchAPI.getHotelRates(hotelIds: hotelIds, checkin: checkin!, checkout: checkout!, adults: 1, guestNationality: "US", currency: "USD") { response, error in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+
+            if response != nil {
+                expectation.fulfill()
+            }
+        }
+        wait(for: [expectation], timeout: 5.0)
     }
 }
